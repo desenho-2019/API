7e55c80f54180d4c44dcab91517c7a8c8da97fcb
João Gabriel <joao_salibaa@hotmail.com>
2019-10-16 23:03:15 -0300

    Creating a exemple view to test Authentication

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import permission_classes
 
-# Create your views here.
+@permission_classes([IsAuthenticated])
+class ExampleView(APIView):
+    #permission_classes = [IsAuthenticated]
+
+    def get(self, request, format=None):
+        content = {
+            'status': 'request was permitted'
+        }
+        return Response(content)
