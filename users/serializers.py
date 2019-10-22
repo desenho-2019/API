7c4b59f0fe467319a11e48fbcc074b6ec8523de3
João Gabriel <joao_salibaa@hotmail.com>
2019-10-21 22:00:57 -0300

    Creating users serializers

--- users/serializers.py
+++ users/serializers.py
@@ -1,5 +1,19 @@
 from rest_framework import serializers
+from users.models import CustomUser
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [ 'id','email','password','name','phone','date_of_birth','gender','nationality','facebook','google','photo']
+
+
+class UserCreateUpdateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(
+        style ={'input_type':'password'}
+    )
+    class Meta:
+        model = CustomUser
+        fields = ['email','password','name','phone','date_of_birth','gender','nationality','facebook','google','photo',]
 
 
 
