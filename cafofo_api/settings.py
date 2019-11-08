99effc7043ee413c99a8f8267be70c7177804ef6
João Gabriel <joao_salibaa@hotmail.com>
2019-11-08 09:26:26 -0300

    Installing and configuring django-heroku

--- cafofo_api/settings.py
+++ cafofo_api/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 import os
 from datetime import timedelta
 
+import django_heroku
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -166,3 +168,6 @@ SIMPLE_JWT = {
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
