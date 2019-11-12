fe3fe9a5ed3bef9d51046fa55e454ee86171c22e
João Gabriel <joao_salibaa@hotmail.com>
2019-11-12 11:17:21 -0300

    Fixing bug after merge
    Signed-off-by: João Gabriel <joao_salibaa@hotmail.com>
    Signed-off-by: Rafael Braganca <rabragancaunb@gmail.com>

--- person/models.py
+++ person/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+from users.models import CustomUser
 from republic.models import Republic
 
 class Person(models.Model):
     first_name = models.CharField(_('First Name'), max_length=60)
     surname = models.CharField(_('Surname'), max_length=60)
     user = models.OneToOneField(
-        User,
+        CustomUser,
+        #related_name = 'email'
         on_delete=models.CASCADE,
         primary_key=True,
-        verbose_name="User"
+        verbose_name="CostumUser"
     )
     Republic = models.ForeignKey(
         Republic,
