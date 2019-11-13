9dda68ab5b90ae97db83cf14f04cb1c468ab606d
João Gabriel <joao_salibaa@hotmail.com>
2019-11-13 20:00:59 -0300

    Configuring cros on setings and ading in to requirements

--- cafofo_api/settings.py
+++ cafofo_api/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'person',
     'vacancy',
     'users',
+    'corsheaders',
 
 ]
 
@@ -56,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'cafofo_api.urls'
