bb912c4900c660008ec08e45ad3894879e99d9ad
João Gabriel <joao_salibaa@hotmail.com>
2019-11-17 14:25:54 -0300

    Configuring cors middleware

--- cafofo_api/settings.py
+++ cafofo_api/settings.py
@@ -57,6 +57,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'cafofo_api.urls'
